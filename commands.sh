96226186d51d534c01755c7f84746c0cb0dda250
Yao <yaofeng@andrew.cmu.edu>
2022-05-02 14:39:36 +0800

    change commands

--- commands.sh
+++ commands.sh
@@ -2,3 +2,7 @@ DDEBACKEND=tensorflow python experiments/ode_system/ode_system.py
 DDEBACKEND=tensorflow python experiments/ode_system/ode_system.py --resample
 DDEBACKEND=tensorflow python experiments/schrodinger/schrodinger.py
 DDEBACKEND=tensorflow python experiments/schrodinger/schrodinger.py --resample
+DDEBACKEND=tensorflow python experiments/beltrami_flow/Beltrami_flow.py
+DDEBACKEND=tensorflow python experiments/beltrami_flow/Beltrami_flow.py --resample
+DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py
+DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --resample
