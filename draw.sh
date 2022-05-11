0f74f43246590113a3be1bc328758399c9b9416b
Yao <y-feng20@mails.tsinghua.edu.cn>
2022-05-11 21:55:13 +0800

    bug fixed

--- draw.sh
+++ draw.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 exp_name=$1
 if [ "$exp_name" == "navier_stokes" ]; then
-  CUDA_VISIBLE_DEVICES=0 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN LWIS --re 100 > experiments/"$exp_name"/draw_100.txt &
-  CUDA_VISIBLE_DEVICES=2 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN LWIS --re 200 > experiments/"$exp_name"/draw_200.txt &
-  CUDA_VISIBLE_DEVICES=4 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN LWIS --re 500 > experiments/"$exp_name"/draw_500.txt &
+  CUDA_VISIBLE_DEVICES=0 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN_100.0 LWIS_100.0 --re 100 > experiments/"$exp_name"/draw_100.txt &
+  CUDA_VISIBLE_DEVICES=2 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN_200.0 LWIS_200.0 --re 200 > experiments/"$exp_name"/draw_200.txt &
+  CUDA_VISIBLE_DEVICES=4 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN_500.0 LWIS_500.0 --re 500 > experiments/"$exp_name"/draw_500.txt &
 else
   CUDA_VISIBLE_DEVICES=6 DDEBACKEND=tensorflow python experiments/"$exp_name"/"$exp_name".py --load PINN LWIS > experiments/"$exp_name"/draw.txt &
 fi
--- draw_all.sh
+++ draw_all.sh
@@ -2,6 +2,6 @@
 CUDA_VISIBLE_DEVICES=0 DDEBACKEND=tensorflow python experiments/ode_system/ode_system.py --load PINN LWIS > experiments/ode_system/draw.txt &
 CUDA_VISIBLE_DEVICES=1 DDEBACKEND=tensorflow python experiments/schrodinger/schrodinger.py --load PINN LWIS > experiments/schrodinger/draw.txt &
 CUDA_VISIBLE_DEVICES=2 DDEBACKEND=tensorflow python experiments/burgers/burgers.py --load PINN LWIS > experiments/burgers/draw.txt &
-CUDA_VISIBLE_DEVICES=3 DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --load PINN_100 LWIS_100 > experiments/navier_stokes/draw_100.txt &
-CUDA_VISIBLE_DEVICES=4 DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --load PINN_200 LWIS_200 > experiments/navier_stokes/draw_200.txt &
-CUDA_VISIBLE_DEVICES=5 DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --load PINN_500 LWIS_500 > experiments/navier_stokes/draw_500.txt &
+CUDA_VISIBLE_DEVICES=3 DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --load PINN_100.0 LWIS_100.0 > experiments/navier_stokes/draw_100.txt &
+CUDA_VISIBLE_DEVICES=4 DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --load PINN_200.0 LWIS_200.0 > experiments/navier_stokes/draw_200.txt &
+CUDA_VISIBLE_DEVICES=5 DDEBACKEND=tensorflow python experiments/navier_stokes/navier_stokes.py --load PINN_500.0 LWIS_500.0 > experiments/navier_stokes/draw_500.txt &
