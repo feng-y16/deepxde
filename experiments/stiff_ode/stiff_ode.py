0fd18a6a709c90f54b2e45f4f8dfdb1401621f05
Yao <yaofeng@andrew.cmu.edu>
2022-06-04 14:37:16 +0800

    change weights

--- experiments/stiff_ode/stiff_ode.py
+++ experiments/stiff_ode/stiff_ode.py
@@ -153,7 +153,7 @@ if len(load) == 0:
     net = dde.nn.FNN(layer_size, activation, initializer)
 
     model = dde.Model(data, net)
-    model.compile("adam", lr=1e-3, metrics=["l2 relative error"], loss_weights=[1, 100])
+    model.compile("adam", lr=1e-3, metrics=["l2 relative error"], loss_weights=[1, 1])
     if resample:
         resampler = dde.callbacks.PDEGradientAccumulativeResampler(period=(epochs // (resample_times + 1) + 1) // 3,
                                                                    sample_num=resample_num, sample_count=resample_times,
@@ -187,7 +187,7 @@ else:
         train_state = info["train_state"]
         resampled_data = info["resampled_data"]
         model = dde.Model(data, net)
-        model.compile("adam", lr=1e-3, metrics=["l2 relative error"], loss_weights=[1, 100])
+        model.compile("adam", lr=1e-3, metrics=["l2 relative error"], loss_weights=[1, 1])
         model.resampled_data = resampled_data
         models[prefix] = model
         losses_test[prefix] = np.array(loss_history.loss_test).sum(axis=1)
