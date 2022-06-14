7010b80bd197d7ef1cea5f6bd673313f984b517f
Yao <yaofeng@andrew.cmu.edu>
2022-06-14 15:23:00 +0800

    change data number

--- experiments/schrodinger/schrodinger.py
+++ experiments/schrodinger/schrodinger.py
@@ -16,12 +16,12 @@ import datetime
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-ep", "--epochs", type=int, default=50000)
-    parser.add_argument("-ntrd", "--num-train-samples-domain", type=int, default=2000)
+    parser.add_argument("-ntrd", "--num-train-samples-domain", type=int, default=1000)
     parser.add_argument("-rest", "--resample-times", type=int, default=4)
-    parser.add_argument("-resn", "--resample-numbers", type=int, default=2000)
+    parser.add_argument("-resn", "--resample-numbers", type=int, default=1000)
     parser.add_argument("-r", "--resample", action="store_true", default=False)
     parser.add_argument("-l", "--load", nargs='+', default=[])
-    parser.add_argument("-d", "--dimension", type=int, default=5)
+    parser.add_argument("-d", "--dimension", type=int, default=10)
     parser.add_argument("--sigma", type=float, default=0.1)
     return parser.parse_known_args()[0]
 
