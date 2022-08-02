9022b71d90e6a559cde18ce15ada5d439c8ddbb3
Yao <yaofeng@andrew.cmu.edu>
2022-08-02 10:43:32 +0800

    change download

--- download.sh
+++ download.sh
@@ -4,7 +4,9 @@ trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM
 host=$1
 experiments=("burgers" "navier_stokes" "schrodinger" "stiff_ode")
 for experiment in "${experiments[@]}"; do
-  rsync -az --include "*.pdf" --exclude "*.*" "$host":~/deepxde/experiments/"$experiment"/ ./experiments/"$experiment"/
-  rsync -az --include "*.txt" --exclude "*.*" "$host":~/deepxde/experiments/"$experiment"/ ./experiments/"$experiment"/
+  rsync -az --include "*.pdf" --exclude "*.*" "$host":~/deepxde/experiments/"$experiment"/ \
+  ./experiments/"$experiment"/
+  rsync -az --include "draw*.txt" --exclude "*.*" "$host":~/deepxde/experiments/"$experiment"/ \
+  ./experiments/"$experiment"/
 done
 echo "download complete"
